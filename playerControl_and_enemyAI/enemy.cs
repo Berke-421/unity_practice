3a5a525b495ee6f0f4ed619032d59b437810307c
Berke-svg1 <berkebicer421@gmail.com>
2026-05-11 11:50:12 +0300

    Update enemy.cs

--- playerControl_and_enemyAI/enemy.cs
+++ playerControl_and_enemyAI/enemy.cs
@@ -1,1 +1,32 @@
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float speed = 3f;
+    public Rigidbody rb;
+    public control player;
+    public Vector3 takip;
+
+    void Start()
+    {
+        if(player == null)
+            player = FindFirstObjectByType<control>();
+        if (rb == null)
+            rb = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        if(player.isAppleCollected) // Starts enemy movement when the object is picked up.
+        {
+            takip = Vector3.MoveTowards( 
+                rb.position, 
+                player.transform.position,
+                speed * Time.fixedDeltaTime
+            );
+
+            rb.MovePosition(takip);
+        }  
+    }
+}
 
