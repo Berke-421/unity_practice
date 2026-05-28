75ca67ba6717d194137f739e616a52e5a9dc500e
Berke-svg1 <berkebicer421@gmail.com>
2026-05-28 20:15:01 +0300

    Create bullet_.cs

---
+++ bullet_logic/my_first_bullet/bullet_.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class bullet_ : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("dusman")) // If it collides with anything except the enemy
+        {
+            Destroy(transform.root.gameObject); // destroy bullet
+        }
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * 5f * Time.fixedDeltaTime; // Move straight forward 
+    }
+}
